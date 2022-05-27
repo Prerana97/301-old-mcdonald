dbd6df2d0defa926f2eb4ac231fb6f2284c703a9
Prerana97 <prerana12897@gmail.com>
2022-05-27 18:31:24 +0000

    Added dropdown menu

--- app.py
+++ app.py
@@ -3,6 +3,44 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
 import pandas as pd
+import numpy as np
+  
+  
+# creating random data through randomint
+# function of numpy.random
+np.random.seed(42)
+  
+random_x = np.random.randint(1, 101, 100)
+random_y = np.random.randint(1, 101, 100)
+  
+plot = go.Figure(data=[go.Scatter(
+    x=random_x,
+    y=random_y,
+    mode='markers',)
+])
+  
+# Add dropdown
+plot.update_layout(
+    updatemenus=[
+        dict(
+            buttons=list([
+                dict(
+                    args=["type", "scatter"],
+                    label="Scatter Plot",
+                    method="restyle"
+                ),
+                dict(
+                    args=["type", "bar"],
+                    label="Bar Chart",
+                    method="restyle"
+                )
+            ]),
+            direction="down",
+        ),
+    ]
+)
+  
+plot.show()
 
 ########### Define your variables ######
 
